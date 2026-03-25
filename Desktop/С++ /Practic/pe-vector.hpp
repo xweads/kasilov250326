68ed80ed28b507e034057c8515fc3619a48b769e
dolenko.aleksandr <dolenko.ayu@edu.spbstu.ru>
2026-03-25 17:01:07 +0300

    hometask

--- Desktop/С++ /Practic/pe-vector.hpp
+++ Desktop/С++ /Practic/pe-vector.hpp
@@ -9,35 +9,36 @@ struct Vector {
     ~Vector();
     Vecotr(const Vector <T> &rhs)= delete;
     Vector(size_t size, const T & value );
+   
 
     Vector < T > & operator=(const Vector <T> & rhs) = delete;
 
     bool isEmpty() const noexcept;
     size_t getSize() const noexcept;
-
+ 
+    //Реализовать + тестирование
+    size_t getCapacity() const noexcept;
     void pushBack(const T &);
     void popBack();
 
     private:
     T * data;
     size_t size_, capacity_;
+    explicit Vector(size_t size);
   };
 }
 template< class T >
-knk::Vector< T >::Vector(size_t size, const T & value):
+knk::Vector< T >::Vector(size_t size):
     data_(size ? new T [size]: nullptr),
     size_(size),
     capacity_(size)
+{}
+template< class T >
+knk::Vector< T >::Vector(size_t size, const T & value):
+   Vector(size)
 {
     for ( size_t i=0; i < size ; ++i){
-        try{
-             data_[i] = value;
-        }
-        catch(...){
-           delete [] data_;
-              throw;
-        }
-        ++size_;
+        data_[i] = value;
     }
 }
 template< class T > 
